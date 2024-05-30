612614c613a3f72a2dbbf3b7a6ff5e74af6cb549
unknown <denzelkinyua11@gmail.com>
2024-05-30 04:53:46 +0300

    Commit: Added some functionality to the index view to show the first 5 questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, you are at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5] # Returns the first 5 questions
+    output = ", ".join([q.question for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s" % question_id)
