9d07540320a514087855e2d2760eeda9cd2d8a97
unknown <denzelkinyua11@gmail.com>
2024-06-03 01:33:14 +0300

    Commit: Added Choice to admin.py, used StackedInline to show the choices

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
 # Makes the poll app modifiable from the admin interface by registering our model here into the site
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3 # Adds three extra columns for our choices every time we access a question
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Add question", {"fields": ["question"]}),
-        ("Date Information", {"fields": ["pub_date"]})
+        ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
