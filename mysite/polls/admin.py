8a87d1b3ebfc7b464c27094cc2824877a766ed40
unknown <denzelkinyua11@gmail.com>
2024-06-03 01:04:37 +0300

    Commit: Used fieldsets[] to refine our admin panel

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,6 +4,9 @@ from .models import Question
 # Register your models here.
 # Makes the poll app modifiable from the admin interface by registering our model here into the site
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question"]
+    fieldsets = [
+        ("Add question", {"fields": ["question"]}),
+        ("Date Information", {"fields": ["pub_date"]})
+    ]
 
 admin.site.register(Question, QuestionAdmin)
