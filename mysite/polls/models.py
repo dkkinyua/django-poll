2b04720e6939aff32e8c266b81d2f1579fac6ed8
unknown <denzelkinyua11@gmail.com>
2024-06-03 02:13:12 +0300

    Commit: Added a @admin.display() decorator to display our was_published_recently() method on admin panel

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
@@ -12,6 +13,12 @@ class Question(models.Model):
     # returns a string representation of "question"
     def __str__(self):
         return self.question
+    
+    @admin.display(
+            boolean= True,
+            ordering="pub_date",
+            description="Published recently?"
+    )
     
     def was_published_recently(self):
         now = timezone.now()
